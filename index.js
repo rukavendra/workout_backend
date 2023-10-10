394de79473a3839f9055bfd10c0104fcdc82fee5
Rukavendra Singh Yadav <rukavendrasinghyadav@gmail.com>
2023-10-10 12:12:29 +0530

    new files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-// const cors = require('cors')
+const cors = require('cors')
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 
